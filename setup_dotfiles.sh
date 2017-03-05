620a63229d2a479854a346cddc33fc7d306a28c6
Maxime Ransan <maxime.ransan@gmail.com>
2017-03-05 15:12:58 +0000

    Improve opam install

---
+++ setup_dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mv $(HOME)/.bashrc $(HOME)/.bashrc.backup
+ln -s $(HOME)/dotfiles.git/vimrc $(HOME)/.vimrc
+ln -s $(HOME)/dotfiles.git/bashrc $(HOME)/.bashrc
+ln -s $(HOME)/dotfiles.git/vim $(HOME)/.vim
+ln -s $(HOME)/dotfiles.git/gitconfig $(HOME)/.gitconfig
+echo "Done..."
